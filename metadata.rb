1572d6bf328278bac0ee5a54304ad54428eb7757
Bryan Baugher <Bryan.Baugher@Cerner.com>
2014-02-14 19:59:03 -0600

    Updated license to MIT

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'serf'
 maintainer       'Bryan Baugher'
 maintainer_email 'Bryan.Baugher@Cerner.com'
-license          'All rights reserved'
+license          'The MIT License (MIT)'
 description      'Installs/Configures serf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
