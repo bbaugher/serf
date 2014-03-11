27902fbbf63df62a2cc0327e79c2f4bed3707f6a
Chris Antenesse <chris@antenesse.net>
2014-03-10 20:33:49 -0700

    moving version from VERSION file to metadata

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'Bryan.Baugher@Cerner.com'
 license          'The MIT License (MIT)'
 description      'Installs/Configures serf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+version          0.5.0
