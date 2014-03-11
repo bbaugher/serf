95c20494d57abcfe0070bee5007a5c0b22c802f0
Chris Antenesse <chris@antenesse.net>
2014-03-10 20:34:21 -0700

    version field is a string, not a float

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'Bryan.Baugher@Cerner.com'
 license          'The MIT License (MIT)'
 description      'Installs/Configures serf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          0.5.0
+version          '0.5.0'
